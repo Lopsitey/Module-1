7250c12c5e8beb5a31ef6c799ff2930b63ac2642
Lopsitey <64274246+Lopsitey@users.noreply.github.com>
2024-10-29 12:57:50 +0000

    Update Text-Based Adventure Game.cpp
    Intro now functional

--- Text-Based Adventure Game/Text-Based Adventure Game/Text-Based Adventure Game.cpp
+++ Text-Based Adventure Game/Text-Based Adventure Game/Text-Based Adventure Game.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <windows.h>
-#define sleep(x) Sleep(x*1000)//makes the sleep function better with a macro
+#include <cstdlib>
+#define sleep(x) Sleep(x*1000)//a sleep function macro with a parameter that takes an integer for seconds to delay the program by
+#define clr() system("cls")
 using namespace std;
 int main()
 {
     cout << "Wow, you're finally awake!" << endl;
     sleep(1);
-    cout << "Test" << endl;
+    cout << "." << endl;
+    sleep(1.5);
+    cout << ".." << endl;
+    sleep(1.8);
+    cout << "..." << endl;
+    sleep(2.18);
+    clr();
+    cout << "Adventurer, is that really you?" << endl;
+    sleep(0.5);
+    cout << "(y/n)" << endl;
+    string x = "";
+    cin >> x;
+    sleep(1);
 }
